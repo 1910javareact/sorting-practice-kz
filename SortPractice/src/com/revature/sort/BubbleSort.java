20da66f8326d9e56607f08cd5dc7f161c1bf0de8
TKSun21 <kietnguyen2016@gmail.com>
2019-11-05 14:57:53 -0500

    Bubble Update

--- SortPractice/src/com/revature/sort/BubbleSort.java
+++ SortPractice/src/com/revature/sort/BubbleSort.java
@@ -1,8 +1,21 @@
 package com.revature.sort;
-
 public class BubbleSort {
-	
 	public static void sort(int[] arr) {
 		// TODO Auto-generated method stub
+		for (int i = 1; i < arr.length - 1; i++) {
+			int max = 0;
+			for (int j = 0; j < arr.length - i; j++) {
+				if (arr [max] < arr[j]) {
+					max =  arr[j];
+					arr[j] = arr [j+1];
+					arr [j +1] = max;
+				}
+			}
+			void PrintArray(int j) {
+				for (int i = 0; i < arr.length; ++i)
+					System.out.println(arr[i] + " ");
+				System.out.println();
+			}
+		}
 	}
 }
